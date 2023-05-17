2ff5a4622bd2d633dd1f90f7bd61d4c0d97e465d
vietphan <phantrungviet98@gmail.com>
2023-05-17 20:20:49 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <div className={'grid gap-10 grid-cols-3 grid-rows-3'}>
-            {data.data.map((e) => (
+            {data.data.map((e: any) => (
                 <div className={'flex flex-row items-center'} key={e.id}>
                     <img
                         className={'mr-9'}
